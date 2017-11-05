6757f8ca6a9e1ea18b0715c0d542b57adf204bff
regien <gerardomalpartidavela@gmail.com>
2017-11-05 00:07:34 -0700

    first

---
+++ oldfash/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmalpart <gmalpart@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/04 19:23:13 by gmalpart          #+#    #+#             */
+/*   Updated: 2017/11/05 00:05:45 by gmalpart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int main (int argc, char **argv)
+{
+	int fd;
+	
+	if (argc != 2)
+	{
+		ft_putstr("usage:\"fillit validfile.txt\n\"");
+	}
+	fd = open(argv[1], O_RDONLY);
+	while ( `)
+}
