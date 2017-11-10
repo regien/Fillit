9e023f54b088f18707f0c19d904e2b376c808558
regien <gerardomalpartidavela@gmail.com>
2017-11-09 23:55:50 -0800

    going to another repo

--- oldfash/main.c
+++ oldfash/main.c
@@ -1,67 +1,90 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmalpart <gmalpart@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/04 19:23:13 by gmalpart          #+#    #+#             */
-/*   Updated: 2017/11/06 15:38:39 by gmalpart         ###   ########.fr       */
+/*   Created: 2017/11/06 15:45:44 by gmalpart          #+#    #+#             */
+/*   Updated: 2017/11/09 23:16:13 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
 
-/*
-	static void print_usage(char *name)
-	{
-	ft_putstr("usage: ");
-	ft_putstr(name);
-	ft_putendl(" file");
-	return ;
-	}
-	*/
+const char example[] = "uso:/example fil1\n";
 
-int		main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int		fd;
-	char	buf[21];
-	int		ret;
+//	int		ret;
+	char	***storage;
+//	char	buf[21];
 
-	if (argc == 2)
+
+	if (argc != 2)
 	{
-		// haz la mierda de fillit
-		if ((fd = open(argv[1], O_RDONLY) < 0))
-		{
-			ft_putendl("Error");
-			return(0);
-		}
+		ft_putstr(example);
+		return(0);
+	}
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+//		if((ret = read(fd, buf, 21)) > 0 )
+//		{
+//			buf[ret] = '\0';
+//			ft_putstr(buf);
+//		}
+		close(fd);
+	}
+	return (0);
+}
+
+void	load_file(int fd, char ***storage)
+{
+	int		ret;
+	char	buf[21];
+	
+	while ((ret = read(fd, buf, 21) > 0))
+	{
+		if((check_tetra(buf)) == 1)
+			return;
 		else
-		{
-			ft_putstr("leyendo");
-			if ((ret = read(fd, buf, 21)) > 0)
-			{
-				buf[ret] = '\0';
-				ft_putstr(buf);
-			}
-			ft_putstr("cerrando");
-			close(fd);
-			ft_putstr(buf);
-			return (0);
-		}
-		return (0);
-		/*	else
-			print_usage(argv[0]);
-			return (0);
-			*/
-		/*
-			if (argc != 2)
-			{
-			ft_putstr("usage:\"fillit validfile.txt\n\"");
-			}
-			fd = open(argv[1], O_RDONLY);
-			while ( )
-			*/
+		
 	}
 }
+
+int		check_tetra(char *str)
+{
+	int i;
+	int contador;
+	int nwline;
+
+	nwline = 4
+	contador = 0;
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '#')
+			contador++;
+		if (str[i] == '\n' && i = nwline)
+		{
+			nwline = nwline + 5;
+			i++;
+		}
+		if (contador >= 5 || str[i] != '.' || str[i] != '#')
+			return (1);
+		i++;
+		if (i == 20 && str[i] == '\0')
+			i++;
+	}
+}
+
+//void	load_shapes (t_list , int fd)
+
+
+/*
+char		***holder
+(holder = malloc(sizeof(char**) * count)
+*/
